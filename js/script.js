310fd7593345ab6ab81a5f523807825f0202ef04
Adrian Żmuda <azmuda@OFFICE.local>
2023-10-08 21:52:41 +0200

    Add change file and ridairect js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
-const removeBtn = document.querySelector('#remove');
+const urlpathame=window.location.pathname;
+const urlorigin=window.location.origin;
+const urlredirect=urlorigin+'/loginpage.html'
 
-const removeAlert = (e) => {
-	e.preventDefault();
-	if (confirm('Czy na pewno chcesz usunąć produkt?')) {
-		// Save it!
-		console.log('Thing was saved to the database.');
-	  } else {
-		// Do nothing!
-		console.log('Thing was not saved to the database.');
-	  }
-};
 
-removeBtn.addEventListener('click', removeAlert);
+if (urlpathame.toString()=='/listofproducts.html')
+{
+	const removeBtn = document.querySelector('#remove');
+
+	const removeAlert = (e) => {
+		e.preventDefault();
+		if (confirm('Czy na pewno chcesz usunąć produkt?')) {
+			// Save it!
+			console.log('Thing was saved to the database.');
+		} else {
+			// Do nothing!
+			console.log('Thing was not saved to the database.');
+		}
+	};
+
+	removeBtn.addEventListener('click', removeAlert);
+}
+
+if (urlpathame.toString()=='/listofproducts.html')
+{
+
+}
+else
+{
+	window.location.replace(urlredirect);
+}
